6857f98de40a5c78c0b0ed7e4679f16c9397831c
Tenshinik <@tenshinik@gmail.com>
2022-12-16 22:36:32 +0800

    Examples examp3 изменения

--- Examples/examp3/Program.cs
+++ Examples/examp3/Program.cs
@@ -2,26 +2,34 @@
 
 [3 7 22 2 78] -> 76
 */
-/*алгоритм, задать массив, провести его по циклу, начать отсчет где мин и макс будут изначально равны нулю.
-Вычленить из массива минимальное и максимальное значение, вычленить из них разницу.
-*/
-
 Console.WriteLine("Задан массив вещественных чисел");
 string ? input = Console.ReadLine();
 var result = input.Split(','); 
+if (result.Length == 1)
+{
+    Console.WriteLine( "Массив не валидный");
+    Environment.Exit(1);
+}
 List<int> int_result = new();
 
 for(int i = 0; i < result.Length; i++)
 {
    int_result.Add(Convert.ToInt32(result[i]));
 }
-int min = 100;
-int max = 0;
-for(int i = 0; i < result.Length; i++)
+int min = int_result[0];
+int max = int_result[0];
+for(int i = 1; i < result.Length; i++)
 {
-    if(int_result[i] > max) max = int_result[i];
-    if(int_result[i]< min) min = int_result[i];
+    if(int_result[i] > max) 
+    {
+        max = int_result[i];
+    }
+    else if(int_result[i]< min)
+    {
+        min = int_result[i];
+    }
 }
 Console.WriteLine("max: " + max);
 Console.WriteLine("min: " + min);
 Console.WriteLine(max - min);
+
